97b4340677f8cc6e34d69865b5851724a465a012
Jonas Bostoen <jonas.bostoen@hotmail.com>
2024-12-28 17:09:06 +0100

    feat: create assistant, vector store, upload documents

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/mempirate/scholar
+
+go 1.22.7
+
+require (
+	github.com/openai/openai-go v0.1.0-alpha.41
+	github.com/pkg/errors v0.9.1
+	github.com/rs/zerolog v1.33.0
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/tidwall/gjson v1.14.4 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/tidwall/sjson v1.2.5 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,208 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/openai/openai-go"
+	"github.com/openai/openai-go/option"
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog"
+)
+
+const ASSISTANT_NAME = "Scholar"
+const VECTOR_STORE_NAME = "ScholarVectorStore"
+const OPENAI_MODEL = "gpt-4o-mini"
+
+type Backend struct {
+	log zerolog.Logger
+
+	client *openai.Client
+
+	assistant *openai.Assistant
+	store     *openai.VectorStore
+}
+
+func NewBackend(apiKey string) *Backend {
+	log := NewLogger("scholar")
+
+	log.Info().Msg("Initializing OpenAI client")
+	client := openai.NewClient(
+		option.WithAPIKey(apiKey),
+		option.WithHeader("OpenAI-Beta", "assistants=v2"),
+	)
+
+	return &Backend{
+		log:    log,
+		client: client,
+	}
+}
+
+func main() {
+	key := os.Getenv("OPENAI_API_KEY")
+	if key == "" {
+		panic("OPENAI_API_KEY is not set")
+	}
+
+	backend := NewBackend(key)
+	log := backend.log
+
+	ctx := context.Background()
+
+	if err := backend.init(ctx); err != nil {
+		log.Fatal().Err(err).Msg("Failed to initialize backend")
+	}
+
+	log.Info().Msg("Backend initialized")
+
+	backend.uploadDocument(ctx, "testdata/test.pdf")
+}
+
+// init initializes the backend by getting or creating the assistant and vector store.
+func (b *Backend) init(ctx context.Context) error {
+	assistant, err := b.getOrCreateAssistant(ctx)
+	if err != nil {
+		return err
+	}
+
+	b.assistant = assistant
+
+	vectorStore, err := b.getOrCreateVectorStore(ctx)
+	if err != nil {
+		return err
+	}
+
+	b.store = vectorStore
+
+	b.log.Debug().Str("assistant_id", b.assistant.ID).Str("store_id", b.store.ID).Msg("Updating assistant with vector store")
+	_, err = b.client.Beta.Assistants.Update(ctx, b.assistant.ID, openai.BetaAssistantUpdateParams{
+		ToolResources: openai.F(openai.BetaAssistantUpdateParamsToolResources{
+			FileSearch: openai.F(openai.BetaAssistantUpdateParamsToolResourcesFileSearch{
+				VectorStoreIDs: openai.F([]string{b.store.ID}),
+			}),
+		}),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (b *Backend) getOrCreateAssistant(ctx context.Context) (*openai.Assistant, error) {
+	b.log.Info().Str("name", ASSISTANT_NAME).Msg("Getting or creating assistant")
+	assistants, err := b.client.Beta.Assistants.List(ctx, openai.BetaAssistantListParams{})
+	if err != nil {
+		return nil, err
+	}
+
+	for _, assistant := range assistants.Data {
+		if assistant.Name == ASSISTANT_NAME && len(assistant.Tools) > 0 && assistant.Tools[0].Type == openai.AssistantToolTypeFileSearch {
+			b.log.Debug().Msg("Existing assistant found with file_search tool")
+			return &assistant, nil
+		}
+	}
+
+	b.log.Debug().Msg("Creating new assistant with file_search tool")
+
+	assistant, err := b.client.Beta.Assistants.New(ctx, openai.BetaAssistantNewParams{
+		Name:         openai.String(ASSISTANT_NAME),
+		Instructions: openai.String("You are a scholarly assistant helping in summarizing articles, papers, and other documents."),
+		Model:        openai.String(OPENAI_MODEL),
+		// Description:   param.Field{},
+		// Metadata:      param.Field{},
+		// Temperature:   param.Field{},
+		Tools: openai.F([]openai.AssistantToolUnionParam{
+			openai.FileSearchToolParam{Type: openai.F(openai.FileSearchToolTypeFileSearch)},
+		}),
+		// TopP:          param.Field{},
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	b.log.Info().Str("id", assistant.ID).Msg("Assistant created")
+
+	return assistant, nil
+}
+
+// getOrCreateVectorStore gets or creates a vector store for the assistant.
+// The vector store is used to store document embeddings for the file search tool of the assistant.
+// It will expire after 30 days of inactivity.
+// https://github.com/openai/openai-go/blob/main/examples/beta/vectorstorefilebatch/main.go
+func (b *Backend) getOrCreateVectorStore(ctx context.Context) (*openai.VectorStore, error) {
+	stores, err := b.client.Beta.VectorStores.List(ctx, openai.BetaVectorStoreListParams{})
+	if err != nil {
+		return nil, err
+	}
+
+	for _, store := range stores.Data {
+		if store.Name == VECTOR_STORE_NAME {
+			b.log.Debug().Msg("Existing vector store found")
+			return &store, nil
+		}
+	}
+
+	vectorStore, err := b.client.Beta.VectorStores.New(
+		ctx,
+		openai.BetaVectorStoreNewParams{
+			ExpiresAfter: openai.F(openai.BetaVectorStoreNewParamsExpiresAfter{
+				Anchor: openai.F(openai.BetaVectorStoreNewParamsExpiresAfterAnchorLastActiveAt),
+				// Expires after 30 days of inactivity
+				Days: openai.Int(30),
+			}),
+			Name: openai.String(VECTOR_STORE_NAME),
+		},
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	b.log.Info().Str("id", vectorStore.ID).Msg("Vector store created")
+
+	return vectorStore, nil
+}
+
+func (b *Backend) uploadDocument(ctx context.Context, path string) error {
+	f, err := os.Open(path)
+	if err != nil {
+		errors.Wrap(err, "failed to open file")
+	}
+
+	b.log.Debug().Str("path", path).Msg("Uploading document")
+
+	vsFile, err := b.client.Beta.VectorStores.Files.UploadAndPoll(ctx, b.store.ID, openai.FileNewParams{
+		File: openai.F[io.Reader](f),
+		// Purpose of the file.
+		Purpose: openai.F(openai.FilePurposeAssistants),
+	}, 100)
+
+	if err != nil {
+		return errors.Wrap(err, "failed to upload document to vector store")
+	}
+
+	b.log.Info().Str("path", path).Str("size", formatBytes(vsFile.UsageBytes)).Str("status", string(vsFile.Status)).Msg("Document uploaded")
+
+	return nil
+}
+
+func formatBytes(bytes int64) string {
+	const KiB = 1024
+	const MiB = KiB * 1024
+	const GiB = MiB * 1024
+
+	if bytes < KiB {
+		return fmt.Sprintf("%d B", bytes)
+	} else if bytes < MiB {
+		return fmt.Sprintf("%.1f KiB", float64(bytes)/KiB)
+	} else if bytes < GiB {
+		return fmt.Sprintf("%.1f MiB", float64(bytes)/MiB)
+	} else {
+		return fmt.Sprintf("%.1f GiB", float64(bytes)/GiB)
+	}
+}
