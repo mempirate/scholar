18c4f01e2394b21c70ddde82605148093fcd4773
Jonas Bostoen <jonas.bostoen@hotmail.com>
2024-12-29 12:20:27 +0100

    feat: first summarizations, testing

--- main.go
+++ main.go
@@ -3,42 +3,15 @@ package main
 import (
 	"context"
 	"fmt"
-	"io"
 	"os"
+	"strings"
 
 	"github.com/openai/openai-go"
-	"github.com/openai/openai-go/option"
-	"github.com/pkg/errors"
-	"github.com/rs/zerolog"
 )
 
 const ASSISTANT_NAME = "Scholar"
 const VECTOR_STORE_NAME = "ScholarVectorStore"
-const OPENAI_MODEL = "gpt-4o-mini"
-
-type Backend struct {
-	log zerolog.Logger
-
-	client *openai.Client
-
-	assistant *openai.Assistant
-	store     *openai.VectorStore
-}
-
-func NewBackend(apiKey string) *Backend {
-	log := NewLogger("scholar")
-
-	log.Info().Msg("Initializing OpenAI client")
-	client := openai.NewClient(
-		option.WithAPIKey(apiKey),
-		option.WithHeader("OpenAI-Beta", "assistants=v2"),
-	)
-
-	return &Backend{
-		log:    log,
-		client: client,
-	}
-}
+const OPENAI_MODEL = openai.ChatModelGPT4oMini
 
 func main() {
 	key := os.Getenv("OPENAI_API_KEY")
@@ -57,152 +30,78 @@ func main() {
 
 	log.Info().Msg("Backend initialized")
 
-	backend.uploadDocument(ctx, "testdata/test.pdf")
-}
+	// backend.uploadDocument(ctx, "testdata/FastPay.pdf")
 
-// init initializes the backend by getting or creating the assistant and vector store.
-func (b *Backend) init(ctx context.Context) error {
-	assistant, err := b.getOrCreateAssistant(ctx)
+	thread, err := backend.client.Beta.Threads.New(ctx, openai.BetaThreadNewParams{})
 	if err != nil {
-		return err
+		panic(err)
 	}
 
-	b.assistant = assistant
-
-	vectorStore, err := b.getOrCreateVectorStore(ctx)
-	if err != nil {
-		return err
-	}
-
-	b.store = vectorStore
-
-	b.log.Debug().Str("assistant_id", b.assistant.ID).Str("store_id", b.store.ID).Msg("Updating assistant with vector store")
-	_, err = b.client.Beta.Assistants.Update(ctx, b.assistant.ID, openai.BetaAssistantUpdateParams{
-		ToolResources: openai.F(openai.BetaAssistantUpdateParamsToolResources{
-			FileSearch: openai.F(openai.BetaAssistantUpdateParamsToolResourcesFileSearch{
-				VectorStoreIDs: openai.F([]string{b.store.ID}),
-			}),
+	// Create a message in the thread
+	_, err = backend.client.Beta.Threads.Messages.New(ctx, thread.ID, openai.BetaThreadMessageNewParams{
+		Role: openai.F(openai.BetaThreadMessageNewParamsRoleUser),
+		Content: openai.F([]openai.MessageContentPartParamUnion{
+			openai.TextContentBlockParam{
+				Type: openai.F(openai.TextContentBlockParamTypeText),
+				Text: openai.String("Can you summarize the FastPay paper? What is special about it?"),
+			},
 		}),
 	})
-
 	if err != nil {
-		return err
+		panic(err)
 	}
 
-	return nil
-}
-
-func (b *Backend) getOrCreateAssistant(ctx context.Context) (*openai.Assistant, error) {
-	b.log.Info().Str("name", ASSISTANT_NAME).Msg("Getting or creating assistant")
-	assistants, err := b.client.Beta.Assistants.List(ctx, openai.BetaAssistantListParams{})
-	if err != nil {
-		return nil, err
-	}
-
-	for _, assistant := range assistants.Data {
-		if assistant.Name == ASSISTANT_NAME && len(assistant.Tools) > 0 && assistant.Tools[0].Type == openai.AssistantToolTypeFileSearch {
-			b.log.Debug().Msg("Existing assistant found with file_search tool")
-			return &assistant, nil
-		}
-	}
-
-	b.log.Debug().Msg("Creating new assistant with file_search tool")
-
-	assistant, err := b.client.Beta.Assistants.New(ctx, openai.BetaAssistantNewParams{
-		Name:         openai.String(ASSISTANT_NAME),
-		Instructions: openai.String("You are a scholarly assistant helping in summarizing articles, papers, and other documents."),
-		Model:        openai.String(OPENAI_MODEL),
-		// Description:   param.Field{},
-		// Metadata:      param.Field{},
-		// Temperature:   param.Field{},
-		Tools: openai.F([]openai.AssistantToolUnionParam{
-			openai.FileSearchToolParam{Type: openai.F(openai.FileSearchToolTypeFileSearch)},
-		}),
-		// TopP:          param.Field{},
-	})
-
-	if err != nil {
-		return nil, err
-	}
-
-	b.log.Info().Str("id", assistant.ID).Msg("Assistant created")
-
-	return assistant, nil
-}
-
-// getOrCreateVectorStore gets or creates a vector store for the assistant.
-// The vector store is used to store document embeddings for the file search tool of the assistant.
-// It will expire after 30 days of inactivity.
-// https://github.com/openai/openai-go/blob/main/examples/beta/vectorstorefilebatch/main.go
-func (b *Backend) getOrCreateVectorStore(ctx context.Context) (*openai.VectorStore, error) {
-	stores, err := b.client.Beta.VectorStores.List(ctx, openai.BetaVectorStoreListParams{})
-	if err != nil {
-		return nil, err
-	}
-
-	for _, store := range stores.Data {
-		if store.Name == VECTOR_STORE_NAME {
-			b.log.Debug().Msg("Existing vector store found")
-			return &store, nil
-		}
-	}
-
-	vectorStore, err := b.client.Beta.VectorStores.New(
-		ctx,
-		openai.BetaVectorStoreNewParams{
-			ExpiresAfter: openai.F(openai.BetaVectorStoreNewParamsExpiresAfter{
-				Anchor: openai.F(openai.BetaVectorStoreNewParamsExpiresAfterAnchorLastActiveAt),
-				// Expires after 30 days of inactivity
-				Days: openai.Int(30),
-			}),
-			Name: openai.String(VECTOR_STORE_NAME),
-		},
-	)
-
-	if err != nil {
-		return nil, err
-	}
-
-	b.log.Info().Str("id", vectorStore.ID).Msg("Vector store created")
-
-	return vectorStore, nil
-}
-
-func (b *Backend) uploadDocument(ctx context.Context, path string) error {
-	f, err := os.Open(path)
-	if err != nil {
-		errors.Wrap(err, "failed to open file")
-	}
-
-	b.log.Debug().Str("path", path).Msg("Uploading document")
-
-	vsFile, err := b.client.Beta.VectorStores.Files.UploadAndPoll(ctx, b.store.ID, openai.FileNewParams{
-		File: openai.F[io.Reader](f),
-		// Purpose of the file.
-		Purpose: openai.F(openai.FilePurposeAssistants),
+	// Create a run
+	run, err := backend.client.Beta.Threads.Runs.NewAndPoll(ctx, thread.ID, openai.BetaThreadRunNewParams{
+		AssistantID:  openai.String(backend.assistant.ID),
+		Instructions: openai.String("You are a scholarly assistant helping in summarizing articles, papers, and other documents. Use your vector store to respond to questions. Be concise to reduce token usage."),
 	}, 100)
 
 	if err != nil {
-		return errors.Wrap(err, "failed to upload document to vector store")
+		panic(err)
 	}
 
-	b.log.Info().Str("path", path).Str("size", formatBytes(vsFile.UsageBytes)).Str("status", string(vsFile.Status)).Msg("Document uploaded")
+	fileCache := make(map[string]string)
 
-	return nil
-}
+	if run.Status == openai.RunStatusCompleted {
+		messages, err := backend.client.Beta.Threads.Messages.List(ctx, thread.ID, openai.BetaThreadMessageListParams{})
 
-func formatBytes(bytes int64) string {
-	const KiB = 1024
-	const MiB = KiB * 1024
-	const GiB = MiB * 1024
+		if err != nil {
+			panic(err.Error())
+		}
 
-	if bytes < KiB {
-		return fmt.Sprintf("%d B", bytes)
-	} else if bytes < MiB {
-		return fmt.Sprintf("%.1f KiB", float64(bytes)/KiB)
-	} else if bytes < GiB {
-		return fmt.Sprintf("%.1f MiB", float64(bytes)/MiB)
+		// The first message is the response
+		message := messages.Data[0]
+
+		content := message.Content[0]
+
+		citations := make([]string, len(content.Text.Annotations))
+
+		for i, annotation := range content.Text.Annotations {
+			index := i + 1
+			content.Text.Value = strings.Replace(content.Text.Value, annotation.Text, fmt.Sprintf(" [%d]", index), 1)
+			citation := annotation.FileCitation.(openai.FileCitationAnnotationFileCitation)
+			if file, ok := fileCache[citation.FileID]; ok {
+				citations[i] = fmt.Sprintf("[%d] %s", index, file)
+				continue
+			}
+
+			file, err := backend.getFileName(ctx, citation.FileID)
+			if err != nil {
+				log.Err(err).Int("index", index).Msg("Invalid file citation, file doesn't exist in vector store")
+				continue
+			}
+
+			fileCache[citation.FileID] = file
+			citations[i] = fmt.Sprintf("[%d] %s", index, file)
+		}
+
+		println(content.Text.Value)
+		println()
+		println(strings.Join(citations, "\n"))
 	} else {
-		return fmt.Sprintf("%.1f GiB", float64(bytes)/GiB)
+		println("Run not completed", run.Status)
+		println(run.JSON.RawJSON())
 	}
+
 }
